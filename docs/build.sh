bf444264331603147efd8e452fc8a83a459c50df
zhouzm2 <zhouzm2@xiaopeng.com>
2021-01-20 18:18:50 +0800

    u

--- docs/build.sh
+++ docs/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd $(dirname $0)
 for old_name in $(cd ..; ls *.md); do {
-    new_name=$(echo "$old_name" | sed 's@\([0-9][0-9]\)@2021-\1-@');
-    ln -svf "../../$old_name" "_posts/$new_name"
+    new_name=$(echo "$old_name" | sed 's@\([0-9][0-9]\)@2021-\1-@')
+    title=$(echo "$old_name" | sed 's@[0-9]\+-\(.*\)\.md@\1@')
+    (echo -e "---\nlayout: post\ntitle:  \"$title\"\n---\n"; cat ../$old_name) > _posts/$new_name
 } done
+jekyll clean
+ln -svf ../docs _site
 jekyll build
 
