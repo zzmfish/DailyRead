22ca4a91326c38ad1b1010409deac3591d1f69c0
zhouzm2 <zhouzm2@xiaopeng.com>
2021-01-23 10:37:51 +0800

    1

--- jekyll/build.sh
+++ jekyll/build.sh
@@ -6,7 +6,7 @@ for old_name in $(cd ..; ls *.md); do {
     title=$(echo "$old_name" | sed 's@[0-9]\+-\(.*\)\.md@\1@')
     (echo -e "---\nlayout: post\ntitle:  \"$title\"\n---\n"; cat ../$old_name) > _posts/$new_name
 } done
-jekyll clean
+bundle exec jekyll clean
 ln -svf ../docs _site
 bundle exec jekyll build
 
